8bc15a1863e437a137b14a28abbf734237b27f46
D254889 <D254889@edu.curio.nl>
2024-01-12 10:41:01 +0100

    Een nodige push

--- PRA_B4_FOTOKIOSK/PRA_B4_FOTOKIOSK/obj/Debug/net6.0-windows/App.g.i.cs
+++ PRA_B4_FOTOKIOSK/PRA_B4_FOTOKIOSK/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace PRA_B4_FOTOKIOSK {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace PRA_B4_FOTOKIOSK {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public static void Main() {
             PRA_B4_FOTOKIOSK.App app = new PRA_B4_FOTOKIOSK.App();
             app.InitializeComponent();
